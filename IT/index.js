864946866b5ff6ab67c6466f0fbe501b14b4cd44
Eves-Residence <evesrespropertycompanyphone@gmail.com>
2025-10-21 17:15:01 +0800

    Refactor changeFrame function for better display handling
    Updated changeFrame function to include display handling and improved opacity transition.

--- IT/index.js
+++ IT/index.js
@@ -1,34 +1,48 @@
-
 // ===============================
 // IFRAME SWITCHER
 // ===============================
 function changeFrame(type) {
   const iframe = document.getElementById("mainFrame");
-  iframe.style.opacity = 0;
 
+  // Initially hide
+  iframe.style.opacity = 0;
+  iframe.style.display = "none";
+
+  // Choose source
+  let newSrc = "";
+  switch (type) {
+    case "master":
+      newSrc = "https://docs.google.com/spreadsheets/d/15ouIKyyo1pfegl7oMxUgNgy_36JPb87Ta4JGxgws5HI/edit?usp=sharing";
+      break;
+    case "search":
+      newSrc = "https://tephdy.github.io/WEB-APP/";
+      break;
+    case "waitlist":
+      newSrc = "https://docs.google.com/spreadsheets/d/1S2v43l75aC6EpyCkXNifSfFYvuXE_XJ9HPcr0RDyhtg/edit?usp=sharing";
+      break;
+    case "bnb":
+      newSrc = "https://docs.google.com/spreadsheets/d/1aWdlIT9aRwT4FktT_3oB0poxC8xyC0lOTDKEj574M2Y/edit?usp=sharing";
+      break;
+    case "calendar":
+      newSrc = "https://calendar.google.com/calendar/embed?src=f8939355c05bdafed63e7eb02789566c7ebe844c36005d3ce552d4d2fd6cba16%40group.calendar.google.com&ctz=Asia%2FManila";
+      break;
+    default:
+      newSrc = "https://tephdy.github.io/WEB-APP/";
+  }
+
+  // Update src
   setTimeout(() => {
-    switch (type) {
-      case "master":
-        iframe.src = "https://docs.google.com/spreadsheets/d/15ouIKyyo1pfegl7oMxUgNgy_36JPb87Ta4JGxgws5HI/edit?usp=sharing";
-        break;  
-      case "search":
-        iframe.src = "https://tephdy.github.io/WEB-APP/";
-        break;
-      case "waitlist":
-        iframe.src = "https://docs.google.com/spreadsheets/d/1S2v43l75aC6EpyCkXNifSfFYvuXE_XJ9HPcr0RDyhtg/edit?usp=sharing";
-        break;
-      case "bnb":
-        iframe.src = "https://docs.google.com/spreadsheets/d/1aWdlIT9aRwT4FktT_3oB0poxC8xyC0lOTDKEj574M2Y/edit?usp=sharing";
-        break;
-      case "calendar":
-        iframe.src = "https://calendar.google.com/calendar/embed?src=f8939355c05bdafed63e7eb02789566c7ebe844c36005d3ce552d4d2fd6cba16%40group.calendar.google.com&ctz=Asia%2FManila";
-        break;
-      default:
-        iframe.src = "https://tephdy.github.io/WEB-APP/";
+    iframe.src = newSrc;
+
+    // Show iframe once it has a valid src
+    if (newSrc && newSrc.trim() !== "") {
+      iframe.style.display = "block";
     }
-    iframe.onload = () => (iframe.style.opacity = 1);
+
+    // Smooth fade-in when loaded
+    iframe.onload = () => {
+      iframe.style.transition = "opacity 0.4s ease";
+      iframe.style.opacity = 1;
+    };
   }, 200);
 }
-
-//task manager
-
