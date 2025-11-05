490a885d9baa3d3898f889d8f883c825caec0861
Eves-Residence <evesrespropertycompanyphone@gmail.com>
2025-11-05 10:35:55 +0800

    Update index.js

--- IT/index.js
+++ IT/index.js
@@ -14,17 +14,33 @@ function changeFrame(type) {
     case "master":
       newSrc = "https://docs.google.com/spreadsheets/d/15ouIKyyo1pfegl7oMxUgNgy_36JPb87Ta4JGxgws5HI/edit?usp=sharing";
       break;
+
     case "search":
       newSrc = "https://tephdy.github.io/WEB-APP/";
       break;
+
     case "waitlist":
       newSrc = "https://docs.google.com/spreadsheets/d/1S2v43l75aC6EpyCkXNifSfFYvuXE_XJ9HPcr0RDyhtg/edit?usp=sharing";
       break;
+
     case "bnb":
       newSrc = "https://docs.google.com/spreadsheets/d/1aWdlIT9aRwT4FktT_3oB0poxC8xyC0lOTDKEj574M2Y/edit?usp=sharing";
       break;
+
     case "calendar":
       newSrc = "https://calendar.google.com/calendar/embed?src=f8939355c05bdafed63e7eb02789566c7ebe844c36005d3ce552d4d2fd6cba16%40group.calendar.google.com&ctz=Asia%2FManila";
+      break;
+
+    case "attendance":
+      newSrc = "https://docs.google.com/forms/d/e/1FAIpQLSfXacHkUdWuQNvv1Pwcyx--NDFqFwjITTYL7672ZL6BG4-SgA/viewform?embedded=true";
+      break;
+
+    case "off":
+      newSrc = "https://docs.google.com/forms/d/e/1FAIpQLSdgQKxcuAsomlhDX6yDsPI1s5O-x-u36-YPtHGGu-33QMMMCQ/viewform?embedded=true";
+      break;
+
+    case "ca":
+      newSrc = "https://docs.google.com/forms/d/e/1FAIpQLSfhw4VyYKI9fc05UGtkvpRx0kIo98QRTKQsH_3NTpZAdzxi4w/viewform?embedded=true";
       break;
     default:
       newSrc = "https://tephdy.github.io/WEB-APP/";
