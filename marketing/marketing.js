1b777740d2b190b7dc7a2173e86c687ae1ce2cf0
Eves-Residence <evesrespropertycompanyphone@gmail.com>
2025-11-16 08:55:43 +0800

    Refactor iframe switcher and task manager

--- marketing/marketing.js
+++ marketing/marketing.js
@@ -48,6 +48,7 @@ function changeFrame(type) {
 
     case "bnb_dates":
       newSrc = "https://calendar.google.com/calendar/embed?src=00c9b4f66e0573f992bb911bb11ddc608ccb021f2be44fa6cfdc633de1463f82%40group.calendar.google.com&ctz=Asia%2FManila";
+      break;
    
     default:
       newSrc = "https://tephdy.github.io/WEB-APP/";
@@ -379,5 +380,6 @@ async function deleteTask(index) {
 
 // ✅ Load tasks on page load
 window.addEventListener("load", fetchTasks);
+
 
 
